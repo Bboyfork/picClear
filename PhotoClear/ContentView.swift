a0a0e7a1b9daed1e6c08e1495bac9cea615884e0
bboyfork <cklothariogm@gmail.com>
2026-06-12 17:54:13 +0800

    Initial Commit

---
+++ PhotoClear/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PhotoClear
+//
+//  Created by fork Tian on 12/06/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PhotoClear/PhotoClearApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoClearApp.swift
+//  PhotoClear
+//
+//  Created by fork Tian on 12/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct PhotoClearApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
